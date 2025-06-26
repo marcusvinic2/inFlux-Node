e6c3764f8ea67958dafa632cebefafc569c1475f
Marcus Vinicius <marcusvinic.br.154@gmail.com>
2025-06-26 10:48:31 -0300

    add novo cnpj para cod cliente neogrid

--- src/tables/cnpj.ts
+++ src/tables/cnpj.ts
@@ -1,5 +1,6 @@
 export const findCodigoCliente = (cnpj: string) => {
   const action_flow_custom_data = [
+    { cnpj: '16882484000260', codigo: '403' },
     { cnpj: '16882484000341', codigo: '404' },
     { cnpj: '16882484000422', codigo: '405' },
     { cnpj: '16882484000503', codigo: '406' },
