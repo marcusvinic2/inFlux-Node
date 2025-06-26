49c63153a76e8240216725653203ffcd3670b386
Marcus Vinicius <marcusvinic.br.154@gmail.com>
2025-06-26 10:40:05 -0300

    alterando ip para ambiente de testes questor

--- src/infra/questor/index.ts
+++ src/infra/questor/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { KeyQuestor } from '../../config/auth'
 
 export const ApiQuestor = axios.create({
-  baseURL: 'https://oak-api.questor.com.br',
+  baseURL: 'http://18.191.21.59:8080',
   headers: {
     Login: KeyQuestor.Login,
     Password: KeyQuestor.Password,
